4bb5065949c7bde4e444207c99e5d9536c04f5b6
airtrans33 <sw10d222@gmail.com>
2024-06-06 21:47:17 +0800

    i
    i

---
+++ config/middlewares.js
@@ -0,0 +1,39 @@
+module.exports = ({ env }) => [
+  "strapi::logger",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "default-src": ["'self'"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            env("SUPABASE_URL"),
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            env("SUPABASE_URL"),
+          ],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
